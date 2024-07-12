6973227921967c493c647f2893e911cf3ab30b4a
omlnaut <oneironaut.oml@gmail.com>
2024-07-12 21:12:31 +0200

    static visualization done

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
+from datetime import datetime
 import os
+import locale
 
 
 def load_openai_key():
     with open("openai.key", "r") as f:
         os.environ["OPENAI_API_KEY"] = f.read().strip()
+
+
+
+def parse_to_date_object(date_str: str) -> datetime:
+    locale.setlocale(locale.LC_TIME, 'de_DE')
+    date_obj = datetime.strptime(date_str, "%d. %B %Y")
+    return date_obj
